c78e79b4edee40ae70b10ff4e40aafdaccb66ca0
Martin-droid <martmnsh@gmail.com>
2021-12-06 12:29:29 +0300

    initial commit

--- my-next-portfolio/pages/index.js
+++ my-next-portfolio/pages/index.js
@@ -41,7 +41,7 @@ const Home = () => (
           <p>Software Developer, Mentor and Trainer</p>
 		  <List ml={4} my={4}>
 		  <ListItem>
-			  <a href="MartinNdegwaResume.pdf">
+			  <a href="Martin_NdegwaResume.pdf">
           <Meta>View Resume</Meta>
 </a>
         </ListItem>
